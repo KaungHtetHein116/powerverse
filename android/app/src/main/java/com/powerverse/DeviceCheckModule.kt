24ddf88c6b14ce1893cf9161c631f8a2f9d5d608
Kaung-Htet-Hein <100679042+Kaung-Htet-Hein@users.noreply.github.com>
2024-04-17 13:32:36 +0630

    fix: android device check

--- android/app/src/main/java/com/powerverse/DeviceCheckModule.kt
+++ android/app/src/main/java/com/powerverse/DeviceCheckModule.kt
@@ -15,10 +15,23 @@ class DeviceCheckModule(reactContext: ReactApplicationContext) : ReactContextBas
 
     @ReactMethod
     fun isEmulator(promise: Promise) {
-        val emulator = (Build.FINGERPRINT.startsWith("google/sdk_gphone")
+        val emulator = Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
                 || Build.MODEL.contains("Emulator")
                 || Build.MODEL.contains("Android SDK built for x86")
-                || Build.MANUFACTURER.contains("Genymotion"))
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.HARDWARE.contains("goldfish")
+                || Build.HARDWARE.contains("ranchu")
+                || Build.HARDWARE.contains("vbox86")
+                || Build.PRODUCT.contains("sdk")
+                || Build.PRODUCT.contains("google_sdk")
+                || Build.PRODUCT.contains("sdk_google")
+                || Build.PRODUCT.contains("sdk_x86")
+                || Build.PRODUCT.contains("vbox86p")
+                || Build.PRODUCT.contains("emulator")
+                || Build.PRODUCT.contains("simulator")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
         promise.resolve(emulator)
     }
 }
